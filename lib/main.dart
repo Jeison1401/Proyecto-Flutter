466173733aebe26f466c02dcbc7ee53315d6f0bf
DESKTOP-UJESM3D\Daniel <dalvisa87@gmail.com>
2021-05-10 10:44:19 -0500

    Cambio del texto a español

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Has pulsado el boton esta cantidad de veces:',
             ),
             Text(
               '$_counter',
